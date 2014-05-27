72cda44e13b44509901309fb547adacb57e5441e
BCorr17 <CorrBradley@gmail.com>
2014-05-27 10:11:02 -0400

    dfghdyzttu

---
+++ Chapter7Problems/Problem1/Pizza.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Problem1
+{
+    class Pizza
+    {
+        string toppings;
+        int inches;
+        double price;
+    }
+}
