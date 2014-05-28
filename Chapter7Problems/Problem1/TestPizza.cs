9558ac2df22aaca0c4dcf63455e8712c253e20a2
BCorr17 <CorrBradley@gmail.com>
2014-05-28 09:46:45 -0400

    dtyjktkustukstyjld

--- Chapter7Problems/Problem1/TestPizza.cs
+++ Chapter7Problems/Problem1/TestPizza.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿                           using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,6 +7,23 @@ namespace Problem1
 {
     class TestPizza
     {
+        private double Pizzamath;
+
+        public TestPizza (double diameter)
+        {
+        }
+        public double diameter
+        {
+            get
+            {
+                return diameter;
+            }
+
+            set
+            {
+                diameter = value;
+            }
+        }
 
     }
 }
