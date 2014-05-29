9275fcc6cb722a6ee4a3d2c56937ea1abf8b602a
BCorr17 <CorrBradley@gmail.com>
2014-05-29 09:33:27 -0400

    wggwrgrqgrqqgr

---
+++ Chapter7Problems/Problem 2/Houseplant.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Problem_2
+{
+    class Houseplant
+    {
+        private double Philodendron;
+
+        public Plant1 (double Philodendron)
+        {
+            this.Philodendron = 29.99;
+    }
+}
